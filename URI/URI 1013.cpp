6467f521466408ab0727c1204c3f2cb975a3c70d
Mehedi Hasan Shuvo <duetinmehedishuvo@gmail.com>
2020-08-23 01:02:01 +0600

    commit

---
+++ URI/URI 1013.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,maxab,max;
+    cin>>a>>b>>c;
+    maxab=(a+b+abs(a-b))/2;
+    max=(maxab+c+abs(maxab-c))/2;
+    cout<<max<<" eh o maior"<<endl;
+    return 0;
+}
---
+++ URI/URI 1014.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double a,b;
+    cin>>a>>b;
+    cout<<fixed<<setprecision(3)<<a/b<<" km/l"<<endl;
+    return 0;
+}
