bad51b224b4f3dd34c7301aea7ef45b6f2c8adc7
Mehedi Hasan Shuvo <duetinmehedishuvo@gmail.com>
2020-08-23 00:31:15 +0600

    commit

---
+++ URI/URI 1011.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double pi=3.14159;
+    int r;
+    cin>>r;
+    cout<<"VOLUME = "<<fixed<<setprecision(3)<<((4*pi*pow(r,3)/3))<<endl;
+    return 0;
+}
