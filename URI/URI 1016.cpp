5be76856dee1fa1ac1da367b57396f95e585c432
Mehedi Hasan Shuvo <duetinmehedishuvo@gmail.com>
2020-08-23 10:25:13 +0600

    commit

---
+++ URI/URI 1016.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int d;
+    cin>>d;
+    int time=d/((90/60.0)-(60/60.0));
+    cout<<time<<" minutos"<<endl;
+    return 0;
+}
