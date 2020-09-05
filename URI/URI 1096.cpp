3f44f6ec7d5686f12b69b45a99cb341530123561
Mehedi Hasan Shuvo <duetinmehedishuvo@gmail.com>
2020-09-05 22:57:39 +0600

    commit

---
+++ URI/URI 1096.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    for(int j=1;j<=9;j+=2){
+        for(int i=7;i>=5;i--){
+            cout<<"I="<<j<<" J="<<i<<endl;
+        }
+    }
+    return 0;
+}
